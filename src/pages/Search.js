121d892d50a775491ae18f7d30698fa92b64d649
Cara Bunnell <carabunnell@gmail.com>
2020-10-20 12:01:10 -0700

    adding col, row, containter to search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
 class Search extends Component {
     state = {
         result: {},
         search: ""
     };
+
+
+render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-8">
+            
+          </Col>
+          <Col size="md-4">
+            
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
 
 export default Search;
