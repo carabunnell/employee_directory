f45e0e43a7617f0e04321bd0a6dbde6b966c2fee
Cara Bunnell <carabunnell@gmail.com>
2020-10-19 20:09:39 -0700

    adding menu, pages, footer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Watchlist from "./pages/Watchlist";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
-// import "./App.css";
 
-class App extends Component {
-  state = {
-    friends
-  };
-
-removeFriend = id => {
-  const friends = this.state.friends.filter(friend => friend.id !== id);
-  this.setState({ friends });
-}
-render() {
+function App() {
   return (
-    <Wrapper>
-      <Title>Movie List</Title>
-      {this.state.friends.map(friend => (
-        <FriendCard
-        removeFriend={this.removeFriend}
-        id={friend.id}
-        key={friend.id}
-        name={friend.name}
-        image={friend.image}
-        occupation={friend.occupation}
-        location={friend.location}
-        />
-      ))}
-    </Wrapper>
-  )
-  }
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Watchlist} />
+          <Route exact path="/watchlist" component={Watchlist} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Watchlist.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import FriendCard from "../components/FriendCard";
+import WrapperOne from "../components/WrapperOne";
+import Title from "../components/Title";
+import friends from "../friends.json";
+// import "./App.css";
+
+
+class Watchlist extends Component {
+  state = {
+    friends
+  };
+
+removeFriend = id => {
+  const friends = this.state.friends.filter(friend => friend.id !== id);
+  this.setState({ friends });
+}
+render() {
+  return (
+    <WrapperOne>
+      <Title>Movie List</Title>
+      {this.state.friends.map(friend => (
+        <FriendCard
+        removeFriend={this.removeFriend}
+        id={friend.id}
+        key={friend.id}
+        name={friend.name}
+        image={friend.image}
+        occupation={friend.occupation}
+        location={friend.location}
+        />
+      ))}
+    </WrapperOne>
+  )
+  }
+}
+
+export default Watchlist;
