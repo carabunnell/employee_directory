90777454d67523ec45028e0b56ecd07f2931c419
Cara Bunnell <carabunnell@gmail.com>
2020-10-20 11:56:18 -0700

    adding search page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Watchlist from "./pages/Watchlist";
+import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -13,6 +14,7 @@ function App() {
         <Wrapper>
           <Route exact path="/" component={Watchlist} />
           <Route exact path="/watchlist" component={Watchlist} />
+          <Route exact path="/search" component={Search} />
         </Wrapper>
         <Footer />
       </div>
---
+++ src/pages/Search.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+    state = {
+        result: {},
+        search: ""
+    };
+}
+
+export default Search;
